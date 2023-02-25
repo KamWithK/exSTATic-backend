90b925a6d506329f0a43adf7cf5097f010cea4ec
Kamron Bhavnagri <kamwithk@tuta.io>
2023-02-25 23:28:56 +1100

    Update npm before running codepipeline synth

--- infrastructure/lib/codepipeline-stack.ts
+++ infrastructure/lib/codepipeline-stack.ts
@@ -11,6 +11,7 @@ export class CodePipelineStack extends Stack {
 
         const synth = new ShellStep('synth', {
             input: CodePipelineSource.connection(PIPELINE_CONFIG.repo_string, PIPELINE_CONFIG.branch, PIPELINE_CONFIG.connection),
+            installCommands: ['npm i -g npm@latest'],
             commands: ['cd infrastructure', 'npm ci', 'npm run build', 'npx cdk synth']
         });
         const pipeline = new CodePipeline(this, 'pipeline', {
