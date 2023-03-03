78b3ce5a394df85a859d5de23417e19a922a1f10
Kamron Bhavnagri <kamwithk@tuta.io>
2023-03-03 22:19:59 +1100

    Settings function pushes changes to DynamoDB

--- functions/go.mod
+++ functions/go.mod
@@ -2,4 +2,9 @@ module github.com/KamWithK/exSTATic-backend
 
 go 1.20
 
-require github.com/aws/aws-lambda-go v1.37.0
+require (
+	github.com/aws/aws-lambda-go v1.37.0
+	github.com/aws/aws-sdk-go v1.44.209
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- functions/settings/main.go
+++ functions/settings/main.go
@@ -1,11 +1,68 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-func HandleRequest() (string, error) {
-	return "Test", nil
+type OptionArguments struct {
+	Username            string   `json:"username" binding:"required"`
+	MediaType           string   `json:"media_type"`
+	ShowOnLeaderboard   *bool    `json:"show_on_leaderboard"`
+	InterfaceBlurAmount *float32 `json:"interface_blur_amount"`
+	MenuBlurAmount      *float32 `json:"menu_blur_amount"`
+	MaxAFKTime          *float32 `json:"max_afk_time"`
+	MaxBlurTime         *float32 `json:"max_blur_time"`
+	MaxLoadLines        *int16   `json:"max_load_lines"`
+}
+
+type TableKey struct {
+	Username  string
+	MediaType string
+}
+
+var sess *session.Session
+var svc *dynamodb.DynamoDB
+
+func init() {
+	sess = session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc = dynamodb.New(sess)
+}
+
+func HandleRequest(ctx context.Context, options OptionArguments) {
+	key := TableKey{
+		Username:  options.Username,
+		MediaType: options.MediaType,
+	}
+	tableKey, keyErr := dynamodbattribute.MarshalMap(key)
+
+	if keyErr != nil {
+		log.Fatalln("Error marshalling key:", keyErr)
+	}
+
+	tableItem, itemErr := dynamodbattribute.MarshalMap(key)
+
+	if itemErr != nil {
+		log.Fatalln("Error marshalling item:", itemErr)
+	}
+
+	_, updateErr := svc.UpdateItem(&dynamodb.UpdateItemInput{
+		TableName:                 aws.String("settingsTable"),
+		Key:                       tableKey,
+		ExpressionAttributeValues: tableItem,
+	})
+
+	if itemErr != nil {
+		log.Fatalln("Error updating DynamoDB item:", updateErr)
+	}
 }
 
 func main() {
