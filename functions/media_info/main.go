57f33dcb4dbb19529947aec12372cc7c4a019d3b
Kamron Bhavnagri <kamwithk@tuta.io>
2023-04-28 22:06:27 +1000

    Media info lambda

--- functions/media_info/main.go
+++ functions/media_info/main.go
@@ -1,11 +1,54 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	dynamo_types "github.com/KamWithK/exSTATic-backend"
 )
 
-func HandleRequest() (string, error) {
-	return "Test", nil
+var sess *session.Session
+var svc *dynamodb.DynamoDB
+
+func init() {
+	sess = session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc = dynamodb.New(sess)
+}
+
+func HandleRequest(ctx context.Context, userMediaEntry dynamo_types.UserMediaEntry) error {
+	var time_now = time.Now().Unix()
+	userMediaEntry.LastUpdate = &time_now
+
+	tableKey, keyErr := dynamodbattribute.MarshalMap(userMediaEntry.Key)
+
+	if keyErr != nil {
+		return fmt.Errorf("Error marshalling key: %s", keyErr.Error())
+	}
+
+	updateExpression, expressionAttributeNames, expressionAttributeValues := dynamo_types.CreateUpdateExpressionAttributes(userMediaEntry)
+
+	_, updateErr := svc.UpdateItem(&dynamodb.UpdateItemInput{
+		TableName:                 aws.String("media"),
+		Key:                       tableKey,
+		UpdateExpression:          aws.String(updateExpression),
+		ExpressionAttributeNames:  expressionAttributeNames,
+		ExpressionAttributeValues: expressionAttributeValues,
+	})
+
+	if updateErr != nil {
+		return fmt.Errorf("Error updating DynamoDB item: %s", updateErr.Error())
+	}
+
+	return nil
 }
 
 func main() {
