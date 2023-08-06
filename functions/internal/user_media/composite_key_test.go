1c2210339dc0067d812052708e543df338839a4d
Kamron Bhavnagri <kamwithk@tuta.io>
2023-08-06 23:03:08 +1000

    Move composite key test to user media module

--- functions/internal/user_media/composite_key_test.go
+++ functions/internal/user_media/composite_key_test.go
@@ -1,91 +1,90 @@
-package main
+package user_media
 
 import (
 	"testing"
 
-	"github.com/KamWithK/exSTATic-backend/internal/user_media"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNoKeys(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("", "")
+	key, date, err := SplitUserMediaCompositeKey("", "")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 }
 
 func TestNoPK(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("", "identifier")
+	key, date, err := SplitUserMediaCompositeKey("", "identifier")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 }
 
 func TestNoSK(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("type#username", "")
+	key, date, err := SplitUserMediaCompositeKey("type#username", "")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 }
 
 func TestEmptyPKFields(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("#", "identifier")
+	key, date, err := SplitUserMediaCompositeKey("#", "identifier")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("a#", "identifier")
+	key, date, err = SplitUserMediaCompositeKey("a#", "identifier")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("#a", "identifier")
+	key, date, err = SplitUserMediaCompositeKey("#a", "identifier")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 }
 
 func TestEmptySKFields(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("type#username", "#")
+	key, date, err := SplitUserMediaCompositeKey("type#username", "#")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("type#username", "a#")
+	key, date, err = SplitUserMediaCompositeKey("type#username", "a#")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("type#username", "#a")
+	key, date, err = SplitUserMediaCompositeKey("type#username", "#a")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 }
 
 func TestInvalidDate(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("type#username", "a#identifier")
+	key, date, err := SplitUserMediaCompositeKey("type#username", "a#identifier")
 	assert.Empty(t, key)
 	assert.Nil(t, date)
 	assert.Error(t, err)
 }
 
 func TestValidDates(t *testing.T) {
-	key, date, err := user_media.SplitUserMediaCompositeKey("type#username", "0#identifier")
+	key, date, err := SplitUserMediaCompositeKey("type#username", "0#identifier")
 	assert.NotEmpty(t, key)
 	assert.EqualValues(t, *date, 0)
 	assert.NoError(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("type#username", "1#identifier")
+	key, date, err = SplitUserMediaCompositeKey("type#username", "1#identifier")
 	assert.NotEmpty(t, key)
 	assert.EqualValues(t, *date, 1)
 	assert.NoError(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("type#username", user_media.ZeroPadInt64(0)+"#identifier")
+	key, date, err = SplitUserMediaCompositeKey("type#username", ZeroPadInt64(0)+"#identifier")
 	assert.NotEmpty(t, key)
 	assert.EqualValues(t, *date, 0)
 	assert.NoError(t, err)
 
-	key, date, err = user_media.SplitUserMediaCompositeKey("type#username", user_media.ZeroPadInt64(101)+"#identifier")
+	key, date, err = SplitUserMediaCompositeKey("type#username", ZeroPadInt64(101)+"#identifier")
 	assert.NotEmpty(t, key)
 	assert.EqualValues(t, *date, 101)
 	assert.NoError(t, err)
