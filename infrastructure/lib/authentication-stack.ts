eb9a4632a9e9b4bc11dc7dabe3ecccfbb5b56a55
Kamron Bhavnagri <kamwithk@tuta.io>
2023-09-28 16:35:30 +1000

    OAuth 2 support

--- infrastructure/lib/authentication-stack.ts
+++ infrastructure/lib/authentication-stack.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { AccountRecovery, UserPool, UserPoolClientIdentityProvider } from 'aws-cdk-lib/aws-cognito';
+import { AccountRecovery, OAuthScope, UserPool, UserPoolClientIdentityProvider } from 'aws-cdk-lib/aws-cognito';
 import { HttpUserPoolAuthorizer } from '@aws-cdk/aws-apigatewayv2-authorizers-alpha';
 
 export interface AuthenticationStackProps extends StackProps {
@@ -33,6 +33,19 @@ export class AuthenticationStack extends Stack {
             authFlows: {
                 userPassword: true,
                 userSrp: true
+            },
+            oAuth: {
+                flows: {
+                    authorizationCodeGrant: true
+                },
+                scopes: [ OAuthScope.OPENID, OAuthScope.PROFILE, OAuthScope.EMAIL ],
+                callbackUrls: [ `http${props.environmentType === 'prod' ? 's://exstatic.io' : '://localhost:5173'}/callback` ],
+                logoutUrls: [ `http${props.environmentType === 'prod' ? 's://exstatic.io' : '://localhost:5173'}` ]
+            }
+        });
+        userPool.addDomain('userPoolDomain', {
+            cognitoDomain: {
+                domainPrefix: `exstatic-${props.environmentType}`
             }
         });
 
